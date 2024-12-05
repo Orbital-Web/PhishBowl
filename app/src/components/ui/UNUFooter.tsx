32fec1761aa391ac548a86d15c541459d07873d0
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2024-12-04 22:41:14 -0500

    fix: typo

--- app/src/components/ui/UNUFooter.tsx
+++ app/src/components/ui/UNUFooter.tsx
@@ -5,7 +5,7 @@ export default function UNUFooter({ ...props }) {
     <div className={styles.unufooter}>
       <img src="/images/logo.png"></img>
       <h3 className="text">
-        This is an initiative lead by the United Nations University
+        This is an initiative led by the United Nations University
       </h3>
     </div>
   );
